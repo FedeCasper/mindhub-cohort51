a25dcc6f1e26ff89757fff2bc8d0ec71e5b3ed8e
NicolasCirulli <nicolascirulli2@gmail.com>
2023-08-31 18:05:32 -0300

    Workshop dom

---
+++ Dom/index.js
@@ -0,0 +1,41 @@
+/* 
+template de la card
+ <div class="card border-primary col-10 col-md-5 col-xl-3">
+                     <img class="card-img-top" src="holder.js/100px180/" alt="Title">
+                     <div class="card-body">
+                       <h4 class="card-title">Title</h4>
+                       <p class="card-text">Text</p>
+                     </div>
+                   </div>
+*/
+
+const contendorCards = document.getElementById('contenedorCards')
+
+function crearCard( obj ){
+    return `<div class="card border-primary col-10 col-md-5 col-xl-3">
+                     <img class="card-img-top" src="${obj.imagen}" alt="Title">
+                     <div class="card-body">
+                       <h4 class="card-title">Nombre:${obj.nombre}</h4>
+                       <p class="card-text">Modulo: ${obj.modulo}</p>
+                     </div>
+             </div>
+             ` 
+}
+
+function crearTemplate( listaMentores ){
+    let template = ""
+    for (const mentor of listaMentores) {
+          template += crearCard( mentor )
+    }
+    return template
+
+}
+
+function imprimirCards( template, id ){
+    let contenedor = document.getElementById(id)
+    contenedor.innerHTML = template
+}
+
+const template = crearTemplate( mentores )
+
+imprimirCards( template, "contenedorCards" )
